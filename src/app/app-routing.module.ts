664e3cc79fb6a9d08c0559eec96cbee714375074
Godwin Jacobs <godwin.jacobs@hm.com>
2020-06-11 23:19:17 +0200

    Added Header with bootstrap classes Added not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ErrorPageComponent } from './errorpage/errorpage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: '', redirectTo: '/auth', pathMatch: 'full'},
+ {path: 'auth', component: AuthComponent},
+ { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page Not Found'} },
+ { path: '**', redirectTo: '/not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
+import { ErrorPageComponent } from './errorpage/errorpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,

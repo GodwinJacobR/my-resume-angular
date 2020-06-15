ec62732c0c0d3c496f3e0eb4deacbe202dfab4a9
Godwin Jacobs <godwin.jacobs@hm.com>
2020-06-15 18:26:13 +0200

    Added login page

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray, ControlContainer } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { forbiddenNameValidator } from '../shared/forbidden-name.directive';
+import { MustMatch } from '../shared/must-match.directive';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +11,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
+  loginForm: FormGroup;
+  isLoginMode = true;
+
+  get inputEmail() { return this.loginForm.get('inputEmail'); }
+  get inputPassword() { return this.loginForm.get('inputPassword'); }
+  get inputPasswordConfirm() { return this.loginForm.get('inputPasswordConfirm'); }
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'inputEmail': new FormControl (null, [
+        Validators.required,
+        Validators.email,
+        forbiddenNameValidator(/test/i)
+      ]),
+      'inputPassword': new FormControl (null, [Validators.required]),
+      'inputPasswordConfirm': new FormControl (null, [Validators.required])
+    }, {
+      validators: [MustMatch('inputPassword', 'inputPasswordConfirm')]
+    });
+    this.setConiditionalValidators(this.isLoginMode);
+  }
+
+  onLogin() {
+    console.log(this.loginForm);
+  }
+
+  onForgotPassword() {
+    console.log('Forgot Password');
+    console.log(this.loginForm);
+  }
+
+  switchSignIn() {
+    this.loginForm.reset();
+    this.isLoginMode = !this.isLoginMode;
+    this.setConiditionalValidators(this.isLoginMode);
+  }
+
+  onSignUp() {
+    console.log(this.loginForm);
+    this.isLoginMode = false;
+    this.loginForm.reset();
+  }
+
+  asyncForbiddenNames(control: FormControl) : Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(()=> {
+        if (control.value === "test@test.com") {
+          resolve({'nameisForbidden': true})
+        }
+        resolve(null);
+      }, 3000)
+    })
+    return promise;
+  }
+
+  setConiditionalValidators(isLoginMode: boolean) {
+
+    const inputPasswordConfirm = this.loginForm.get('inputPasswordConfirm');
+    if (isLoginMode) {
+      this.loginForm.setValidators(null);
+      inputPasswordConfirm.setValidators(null);
+
+      this.loginForm.updateValueAndValidity();
+      inputPasswordConfirm.updateValueAndValidity();
+    }
+    else {
+      this.loginForm.setValidators(MustMatch('inputPassword', 'inputPasswordConfirm'));
+      this.loginForm.updateValueAndValidity();
+    }
   }
 
 }
---
+++ src/app/shared/must-match.directive.ts
@@ -0,0 +1,29 @@
+import { FormGroup, AbstractControl } from '@angular/forms';
+
+export function MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+      console.log(formGroup);
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+          // return if another validator has already found an error on the matchingControl
+          return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+          return {'passwordNotMatching': {value: control.value}}
+      } else {
+          return;
+      }
+  }
+
+  // return (control: AbstractControl): {[key: string]: any} | null => {
+
+  //   const control = formGroup.controls[controlName];
+  //   const matchingControl = formGroup.controls[matchingControlName];
+  //   if (control.value !== matchingControl.value)
+  //   return forbidden ? {'forbiddenName': {value: control.value}} : null;
+  // };
+}
